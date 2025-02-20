c242331b428c1544e9794352ce8a54d140dfac47
FedericoIglesias <igl.fede@gmail.com>
2025-02-20 13:27:34 +0100

    featCutImg

---
+++ internal/img/img.go
@@ -0,0 +1,27 @@
+package img
+
+import (
+	"image"
+	"image/png"
+	_ "image/png"
+	"os"
+)
+
+type SubImager interface {
+	SubImage(r image.Rectangle) image.Image
+}
+
+func CutImage(sizeY, sizeX, posY, posX int) image.Image {
+	file, err := os.Open("internal/img/pacmanSprite.png")
+	if err != nil {
+		panic(err)
+	}
+	img, err := png.Decode(file)
+	if err != nil {
+		panic(err)
+	}
+
+	rect := image.Rect(0, 0, sizeX, sizeY)
+	rect = rect.Add(image.Point{posX, posY})
+	return img.(SubImager).SubImage(rect)
+}
